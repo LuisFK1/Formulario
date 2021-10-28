11281245fe6e5d36736c90e3c601c8f68f829b66
LuisFK1 <lfm74304@udelasalle.edu.mx>
2021-10-28 18:47:40 -0500

    objeto creado y con cambios dinamicos

--- JS/formulario.js
+++ JS/formulario.js
@@ -7,13 +7,19 @@ function Destino(estado,distancia,imagen) {
 
 
 //obtencion de los elementos mediante el ID esta es la parte disfuncional
-const prueba = document.getElementById("cambiar");
+const opcion = document.getElementById("opcion");
 const boton = document.getElementById("ejecutar");
+const imagenEstado = document.getElementById("imagen")
 
 //onclick desde javascript
 // boton.onclick = alert("Hola mundo");
 
 //Funcion cotizar la cual es llamada desde el html y 
 function Cotizar() {
-    console.log(prueba.textContent);
+        var textoSeleccionado = [].filter.call(opcion.options, option => option.selected).map(option => option.text);
+        var NumeroSeleccionado = [].filter.call(opcion.options, option => option.selected).map(option => option.value);
+        var ImagenSeleccionada = textoSeleccionado + ".jpg"
+        var lugar = new Destino(textoSeleccionado,NumeroSeleccionado,ImagenSeleccionada);    
+        imagenEstado.src = 'ESTADOS/' + lugar.imagen ;
+        alert(lugar.estado + " " + lugar.distancia + "Km");
 }
